8a28d2482aa580dbcf3734e0151fbdeb831620c2
iratze <iratze.eizaguirre@gmail.com>
2023-11-19 01:32:19 +0000

    Más cambios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import QRComponent from './components/QRCode';
-import QRScanner from './components/QRScanner'
+import QRScanner from './components/QRScanner';
+import { useState } from 'react';
 
 function App() {
 
+  let [success, setSuccess] = useState(null);
+
   const handleScanSuccess = (data) => {
+    setSuccess('DETECTED!!!!!')
     console.log('Scanned data:', data);
   };
 
@@ -15,6 +19,7 @@ function App() {
 
   return (
     <div className="App">
+      { success && <h1>SUCCESS!!!!!</h1> }
       <h1>Escáner de Códigos QR</h1>
       <div className='scanner'>
         <QRScanner
